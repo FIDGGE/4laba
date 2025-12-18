d77c4f47e06a5697fc3311e5e87027b554765a24
FIDGGE <megamix0704@gmail.com>
2025-12-19 00:34:48 +0700

    Create 4laba.cpp

---
+++ 4laba.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  int size;
+  
+  cout << "Enter matrix size: ";
+  cin  >> size;
+  
+  int matrix[10][10];
+  
+  for (int row = 0; row < size; row++) {
+    for (int col = 0; col < size; col++) {
+      cout << "Enter element [" << row << "][" << col << "]: ";
+      cin  >> matrix[row][col];
+    }
+  }
+  
+  cout << "\nMatrix:\n";
+  for (int row = 0; row < size; ++row) {
+    for (int col = 0; col < size; ++col) {
+      cout << matrix[row][col] << " ";
+    }
+    cout << endl;
+  }
+  
+  int sum = 0;
+  int count = 0;
+  
+  for (int row = 0; row < size; ++row) {
+    for (int col = row + 1; col < size; ++col) {
+      if (matrix[row][col] > 0) {
+        sum += matrix[row][col];
+        ++count;
+      }
+    }
+  }
+  
+  cout << "\nSum of positive elements above main diagonal: " << sum << endl;
+  cout << "Count of positive elements above main diagonal: " << count << endl;
+  
+  return 0;
+}
